12cd7740eb7851ff581be2e12237b241e207aa49
Kai Wohlfahrt <kai@charmtx.com>
2023-08-08 23:54:03 +0200

    Getting familiar with kopf
    This creates a very basic operator, that creates a pod owned by the CRD,
    and tests that there are events for that pod.

---
+++ src/dask_operator/operator.py
@@ -0,0 +1,51 @@
+import os
+
+import kopf
+
+from kubernetes_asyncio.config import (
+    load_kube_config,
+    load_incluster_config,
+    ConfigException,
+)
+from kubernetes_asyncio.client import Configuration, ApiClient
+from kubernetes_asyncio import client
+
+
+@kopf.on.login()
+async def login(memo: kopf.Memo, **kwargs):
+    config = Configuration()
+    try:
+        await load_incluster_config(client_configuration=config)
+    except ConfigException:
+        await load_kube_config(os.environ["KUBECONFIG"], client_configuration=config)
+    conn = kopf.login_with_service_account(**kwargs) or kopf.login_with_kubeconfig(
+        **kwargs
+    )
+    memo["api"] = ApiClient(configuration=config)
+    return conn
+
+
+@kopf.on.cleanup()
+async def logout(memo: kopf.Memo, **kwargs):
+    await memo["api"].close()
+
+
+@kopf.on.create("charmtx.com", "daskclusters")
+async def create_cluster(memo, logger, **kwargs):
+    v1 = client.CoreV1Api(memo["api"])
+    adoptee = {}
+    kopf.adopt(adoptee)
+    pod = client.V1Pod(
+        spec=client.V1PodSpec(
+            containers=[
+                client.V1Container(
+                    name="ubuntu",
+                    image="ubuntu:22.04",
+                    command=["bash", "-c"],
+                    args=["trap : TERM INT; sleep infinity & wait"],
+                )
+            ]
+        ),
+        **adoptee
+    )
+    await v1.create_namespaced_pod(pod.metadata["namespace"], pod)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,24 @@
+import asyncio
 from pathlib import Path
+import time
 import pytest
 import yaml
 from yarl import URL
+from kopf.testing import KopfRunner
+from kubernetes_asyncio import client, watch
+from kubernetes_asyncio.client import Configuration, ApiClient
+from kubernetes_asyncio.config import load_kube_config
 
-from dask_operator.__main__ import main
+import dask_operator
+import dask_operator.operator
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="session")
@@ -19,6 +34,61 @@ def kubeconfig(tmp_path_factory):
         yield out_path
 
 
-def test_main(monkeypatch, event_loop, kubeconfig):
-    monkeypatch.setenv("KUBECONFIG", str(kubeconfig.absolute()))
-    event_loop.run_until_complete(main())
+@pytest.fixture(scope="session")
+async def api(kubeconfig):
+    config = Configuration()
+    await load_kube_config(str(kubeconfig), client_configuration=config)
+    async with ApiClient(configuration=config) as api:
+        yield api
+
+
+@pytest.fixture(scope="session")
+async def crd(api: ApiClient):
+    crd_file = Path(dask_operator.__file__).parent / "daskcluster.yaml"
+    crd = yaml.safe_load(crd_file.read_text())
+
+    apiextensions = client.ApiextensionsV1Api(api)
+    r = await apiextensions.create_custom_resource_definition(crd)
+    yield r
+    await apiextensions.delete_custom_resource_definition(r.metadata.name)
+
+
+@pytest.fixture(scope="session")
+def operator(kubeconfig):
+    with pytest.MonkeyPatch.context() as ctx:
+        ctx.setenv("KUBECONFIG", str(kubeconfig.absolute()))
+        with KopfRunner(["run", "--all-namespaces", "-m", "dask_operator"]) as runner:
+            time.sleep(0.01)  # Otherwise, there is an error on teardown
+            yield runner
+    assert runner.exit_code == 0
+
+
+async def test_create(crd, operator, api: ApiClient):
+    custom = client.CustomObjectsApi(api)
+    r = await custom.create_namespaced_custom_object(
+        "charmtx.com",
+        "v1alpha1",
+        "default",
+        "daskclusters",
+        {
+            "apiVersion": "charmtx.com/v1alpha1",
+            "kind": "DaskCluster",
+            "metadata": {"generateName": "test-"},
+            "spec": {"image": "foo", "replicas": 1},
+        },
+    )
+    v1 = client.CoreV1Api(api)
+    w = watch.Watch()
+    async for event in w.stream(
+        v1.list_namespaced_pod, namespace=r["metadata"]["namespace"], timeout_seconds=10
+    ):
+        assert event["object"].metadata.name.startswith("test-")
+        w.stop()
+
+    await custom.delete_namespaced_custom_object(
+        "charmtx.com",
+        "v1alpha1",
+        r["metadata"]["namespace"],
+        "daskclusters",
+        r["metadata"]["name"],
+    )
